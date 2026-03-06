31a2ef54865627a41586f11746c5c20ef82e28dd
choung1295 <choung1295@gmail.com>
2026-03-06 16:28:27 +0900

    fix import

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import Navbar from "@/components/landing/Navbar"
-import HeroSection from "@/components/landing/HeroSection"
-import FeaturesSection from "@/components/landing/FeaturesSection"
-import CTASection from "@/components/landing/CTASection"
+import Navbar from "../components/landing/Navbar"
+import HeroSection from "../components/landing/HeroSection"
+import FeaturesSection from "../components/landing/FeaturesSection"
+import CTASection from "../components/landing/CTASection"
 
 export default function Page() {
     return (
